8ee6e8afd58f8591a104cc8aa90486f7a73d5dfb
Kai-Chih Tseng <ktseng@cake.fios-router.home>
2022-02-19 13:05:31 -0500

    Your message about the commit

--- submit2.sh
+++ submit2.sh
@@ -2,6 +2,6 @@
 git init
 git add . 
 git commit -m "Your message about the commit"
-git remote add origin https://github.com/kuiper2000/chaos_and_predictability.git
+git remote add origin https://github.com/kuiper2000/chaos_and_predictability
 git push -u origin gh-pages
 git push origin gh-pages  
